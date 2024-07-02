6a1a22161acc08c6e2d737b41eea4f36d877e7bb
Diogo Carvalho <diogocvc@gmail.com>
2024-07-02 17:33:05 -0300

    first commit

--- main.js
+++ main.js
@@ -1,63 +1,72 @@
-const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
-const atividades = [];
-const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
+const form = document.getElementById('form-entrada');
+const contatos = []; // Array para armazenar os contatos
 
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    adicionaLinha();
-    atualizaTabela();
-    atualizaMediaFinal();
+    adicionaContato();
+    atualizaLista();
+    totalContatos();
 });
 
-function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+function adicionaContato() {
+    const nomeContato = document.getElementById('nome').value;
+    const numeroContato = document.getElementById('numero').value;
 
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+    if (existeContato(nomeContato, numeroContato)) {
+        alert(`O contato ${nomeContato} ou o número ${numeroContato} já foram inseridos`);
     } else {
 
-        atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade.value));
+        contatos.push({
+            nome: nomeContato,
+            numero: numeroContato
 
+        });
+
+
+        atualizaLinhas();
+
+
+        document.getElementById('nome').value = '';
+        document.getElementById('numero').value = '';
+    }
+
+    totalContatos();
+
+}
+
+function atualizaLinhas() {
+    linhas = '';
+    contatos.forEach((contato, index) => {
         let linha = `<tr>`;
-        linha += `<td>${inputNomeAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += `<td>${contato.nome}</td>`;
+        linha += `<td>${contato.numero}</td>`;
+        linha += `<td><button onclick="excluirContato(${index})">Excluir</button></td>`;
         linha += `</tr>`;
-
         linhas += linha;
-    }
-
-    inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';
+    });
 }
 
-function atualizaTabela(){
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+function atualizaLista() {
+    const corpoLista = document.querySelector('tbody');
+    corpoLista.innerHTML = linhas;
 }
 
-function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
+function existeContato(nome, numero) {
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+    return contatos.some(contato => contato.nome === nome || contato.numero === numero);
 }
 
-function calculaMediaFinal () {
-    let somaDasNotas = 0;
+function excluirContato(index) {
+    contatos.splice(index, 1); 
+    atualizaLinhas(); 
+    atualizaLista(); 
+    totalContatos();
+}
 
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i];
-    }
-    return somaDasNotas / notas.length;
+function totalContatos() {
+    const total = contatos.length;
+    document.getElementById("total").textContent = `Total de contatos: ${total}`;
 }
